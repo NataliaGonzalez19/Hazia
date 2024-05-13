e0f59437e889072ee5b74f6e082919622c4cc052
Angie Natalia Gonzalez Arias <gonzalezariasangienatalia@gmail.com>
2024-05-13 12:27:04 -0500

    Se añade el modulo administrador

---
+++ src/models/administrador.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const administradorSchema = mongoose.Schema({
+
+    nombreSemillero: {
+        type: String,
+        require: true,
+    },
+    descripcion: {
+        type: String,
+        require: true,
+    },
+    liderSemillero: {
+        type: String,
+        require: true,
+    },
+    fechaCreacion: {
+        type: Date,
+        require: true,
+    },
+    estado: {
+        type: Boolean,
+        require: true,
+    },
+    participantes: {
+        type: Date,
+        require: true,
+    },
+
+
+});
+
+module.exports = mongoose.model("Admnistrador", administradorSchema);
