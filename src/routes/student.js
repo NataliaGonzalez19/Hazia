a1784eb3e33bbf3b1e6f2dec3de11603d01dcc05
David <davidale2704@gmail.com>
2024-05-13 21:39:18 -0500

    Peticiones de seeds(semilleros) Post , get, read and delete

--- src/routes/student.js
+++ src/routes/student.js
@@ -31,7 +31,30 @@ token,
 });
 
 
+  //  Validador inicio de sesion "Access estudiante"
 
+
+//inicio de sesión
+
+router.post("/access", async (req, res) => {
+    // validaciones
+    const { error } = perfilalumnoSchema.validate(req.body.codigo, req.body.clave);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+    //Buscando el usuario por su dirección de correo
+    const perfilalumno = await perfilalumnoSchema.findOne({ codigo: req.body.codigo });
+    //validando si no se encuentra
+    if (!perfilalumno) return res.status(400).json({ error: "Usuario no encontrado" });
+    //Transformando la contraseña a su valor original para 
+    //compararla con la clave que se ingresa en el inicio de sesión
+    const validPassword = await bcrypt.compare(req.body.clave, perfilalumno.clave);
+    if (!validPassword)
+    return res.status(400).json({ error: "Clave no válida" });
+    res.json({
+    error: null,
+    data: "Bienvenido alumno(a)"
+     });
+    });
+    
 
 });
 module.exports = router;
